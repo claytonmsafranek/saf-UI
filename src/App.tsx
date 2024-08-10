c2ee869dbc02f8e168f4810f96df5e150bb39258
Clayton Safranek <claytonsafranek@gmail.com>
2024-08-10 09:19:48 -0500

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <h1>Saftware.dev, coming soon...</h1>
+    <Routes>
+      <Route path='/' element={<h1>Saftware.dev, coming soon...</h1>} />
+      <Route path='/test' element={<h1>TEST PAGE</h1>} />
+    </Routes>
   )
 }
 
