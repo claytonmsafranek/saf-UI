04f9a6af8bdce2bc86e944707af3e7fd19dd1e7d
Clayton Safranek <claytonsafranek@gmail.com>
2025-10-12 15:53:09 -0500

    fix build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
- 
+ build: {
+    rollupOptions: {
+      input: {
+        main:   path.resolve(__dirname, 'index.html'),
+        galaga: path.resolve(__dirname, 'galaga.html'),
+      },
+    },
+  }
 })
